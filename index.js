4efd1c915ce98621965d6800b83b0c0f3f7f7e68
Anthony-D99 <anthonydisaf@yahoo.com>
2023-06-28 13:56:53 -0400

    completed and tested for errors in the index.js file, everything seems to be running as expected.

--- index.js
+++ index.js
@@ -3,61 +3,62 @@ const fs = require('fs')
 const connection = require('./config/connection')
 
 
-function init(){
-inquirer.prompt([
-   {
-      type: 'list',
-      name: 'choices',
-      message: 'What would you like to do?',
-      choices: ['view all departments','view all roles', 'view all employees','add a department','add a role','add an employee','update an employee role'],
-   }
-   
-  
-]).then(result => {
-
-   if (result.choices === 'view all departments') {
-      viewDepartments()
-   } else if (result.choices === 'view all roles') {
-      viewRoles()
-   } else if(result.choices === 'view all employees'){
-      viewEmployees()
-   }else if(result.choices === 'add a department'){
-
-   }else if(result.choices === 'add a role'){
-      
-   }else if(result.choices === 'add an employee'){
-      
-   }else{
-
-   }
-
-   
-  
-
-})}
+function init() {
+   inquirer.prompt([
+      {
+         type: 'list',
+         name: 'choices',
+         message: 'What would you like to do?',
+         choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role'],
+      }
 
 
-function viewDepartments(){
-   connection.query('SELECT * FROM department', (err, res)=>{
-      if(err){
+   ]).then(result => {
+
+      if (result.choices === 'view all departments') {
+         viewDepartments()
+      } else if (result.choices === 'view all roles') {
+         viewRoles()
+      } else if (result.choices === 'view all employees') {
+         viewEmployees()
+      } else if (result.choices === 'add a department') {
+         addDepartment()
+      } else if (result.choices === 'add a role') {
+         addRole()
+      } else if (result.choices === 'add an employee') {
+         addEmployee()
+      } else {
+         updateRole()
+      }
+
+
+
+
+   })
+}
+
+
+function viewDepartments() {
+   connection.query('SELECT * FROM department', (err, res) => {
+      if (err) {
          throw err
       }
       console.table(res)
       init()
    })
 }
-function viewRoles(){
-   connection.query('SELECT * FROM role', (err, res)=>{
-      if(err){
+function viewRoles() {
+   connection.query('SELECT * FROM role', (err, res) => {
+      if (err) {
          throw err
       }
       console.table(res)
       init()
    })
 }
-function viewEmployees(){
-   connection.query('SELECT * FROM employee', (err, res)=>{
-      if(err){
+function viewEmployees() {
+   connection.query('SELECT * FROM employee', (err, res) => {
+      if (err) {
          throw err
       }
       console.table(res)
@@ -65,6 +66,107 @@ function viewEmployees(){
    })
 }
 
+function addDepartment() {
+   inquirer.prompt([
+      {
+         type: 'text',
+         name: 'department',
+         message: 'What is the name of the department',
+      }
+   ]).then(result => {
+      connection.query(`INSERT INTO department(name) VALUES ('${result.department}')`, (err, res) => {
+         if (err) {
+            throw err
+         }
+         console.log(`Added ${result.name} to the database`)
+         init()
+      })
+   })
+}
+
+function addRole() {
+   inquirer.prompt([
+      {
+         type: 'text',
+         name: 'role',
+         message: 'What is the name of the role?',
+      },
+      {
+         type: 'number',
+         name: 'salary',
+         message: 'What is the salary of the role?'
+      },
+      {
+         type: 'number',
+         name: 'department',
+         message: 'What is the department number for this role?'
+      },
+   ]).then(result => {
+      connection.query(`INSERT INTO role(title, salary,department_id) VALUES ('${result.role}',${result.salary},${result.department})`, (err, res) => {
+         if (err) {
+            throw err
+         }
+         console.log(`Added ${result.role} to the database`)
+         init()
+      })
+   })
+}
+
+function addEmployee() {
+   inquirer.prompt([
+      {
+         type: 'text',
+         name: 'firstName',
+         message: "What is the emmployee's first name?"
+      },
+      {
+         type: 'text',
+         name: 'lastName',
+         message: "What is the employee's last name?"
+      },
+      {
+         type: 'number',
+         name: 'role',
+         message: "What is the employee's role id number?"
+      },
+      {
+         type: 'text',
+         name: 'manager',
+         message: "What is the employee's manager id number?(Type NULL if none)"
+      },
+   ]).then(result => {
+      connection.query(`INSERT INTO employee(first_name, last_name,role_id,manager_id) VALUES ('${result.firstName}','${result.lastName}',${result.role},${result.manager})`, (err, res) => {
+         if (err) {
+            throw err
+         }
+         console.log(`Added ${result.firstName} ${result.lastName} to the database`)
+         init()
+      })
+   })
+}
+
+function updateRole(){
+   inquirer.prompt([
+      {
+         type: 'number',
+         name:'employee',
+         message:"What is the employee's id number?"
+      },
+      {
+         type: 'number',
+         name:'role',
+         message:"What is the role id you would like to assign to the selected employee?"
+      }
+   ]).then(result =>{
+      connection.query(`UPDATE employee SET role_id = ${result.role} WHERE id = ${result.employee}`, (err, res) => {
+         if (err) {
+            throw err
+         }
+         console.log(`Updated ${result.employee} role to ${result.role}`)
+         init()
+   })
+})
+}
 
 
 init()
